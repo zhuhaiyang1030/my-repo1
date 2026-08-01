da98d242134a2a194f8e08d34a2e5703945510d3
zhuhaiyang <zhuhaiyang@gmail.com>
2026-08-01 13:57:30 +0800

    vscode提交

--- src/main/java/com/example/test/controller/My2Controller.java
+++ src/main/java/com/example/test/controller/My2Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class My2Controller {
 
-    @GetMapping("/hello")
+    @GetMapping("/hello2")
     public String hello() {
 
         return "111";
--- src/main/java/com/example/test/controller/My4Controller.java
+++ src/main/java/com/example/test/controller/My4Controller.java
@@ -1,7 +1,18 @@
 package com.example.test.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class My4Controller {
+    @Value("${appname}")
+    String name;
+
+    @GetMapping("/test")
+    public String test(){
+        return this.name;
+    }
+
+
 }
